e0f8edf64215a55d0dcb403412ff6c6328a11344
Carlos <carlitosmh1272@gmail.com>
2025-06-18 11:44:47 +0200

    Auto update on save

--- auto_push.ps1
+++ auto_push.ps1
@@ -6,13 +6,9 @@ Write-Host "--- Ejecutando auto_push.ps1 ---"
 # Verifica si hay cambios
 $gitStatus = git status --porcelain
 if ($gitStatus) {
-    Write-Host ">>> Cambios detectados, haciendo commit y push..."
     git add .
-    Write-Host "Comando: git add ."
     git commit -m "Auto update on save"
-    Write-Host "Comando: git commit -m 'Auto update on save'"
     git push origin main
-    Write-Host "Comando: git push origin main"
 } else {
     Write-Host ">>> No hay cambios, no se hace nada."
 }
