4d382b136e6296b5b3672731afeb8b0dd96dcbe9
Carlos <carlitosmh1272@gmail.com>
2025-06-20 12:19:37 +0200

    Auto update on save

--- Pages/Movimientos/movimientos.js
+++ Pages/Movimientos/movimientos.js
@@ -1008,6 +1008,8 @@ $(function () {
           </div>
         </li>
       `);
+      
+      if (window.lucide) lucide.createIcons();
 
       // Filtrar por texto
       let filtered = dataList;
