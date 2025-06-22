fd5b8cef47d28e92e361834c85799b429ca45985
Carlos <carlitosmh1272@gmail.com>
2025-06-22 13:28:26 +0200

    Auto update on save

--- Pages/Componentes/user.php
+++ Pages/Componentes/user.php
@@ -105,10 +105,12 @@
             <div class="conceptos-col">
                 <div class="conceptos-title">Ingresos / Gasto</div>
                 <div class="input-container">
-                <select id="tipoMovimientoDefault">
-                    <option value="ingreso">Ingreso</option>
-                    <option value="gasto">Gasto</option>
-                </select>
+                    <div id="tipoMovimientoDisplay" class="concepto-display">Gasto</div>
+                        <ul id="tipoMovimientoOptions" class="concepto-options">
+                            <li data-value="gasto">Gasto</li>
+                            <li data-value="ingreso">Ingreso</li>
+                        </ul>
+                    </div>
                 <p style="font-size: 13px; color: gray; margin-top: 6px;">
                     Si eliges la opción de Ingreso, se activará por defecto la selección de Ingreso al añadir un movimiento.
                 </p>
