bae9226be12f2876a51103a6193f116805179bf0
Carlos <carlitosmh1272@gmail.com>
2025-09-02 13:02:00 +0200

    Auto update on save

--- Pages/Componentes/Assets/userAvatar/usuario_dinero.php
+++ Pages/Componentes/Assets/userAvatar/usuario_dinero.php
@@ -51,6 +51,7 @@ if ($action === 'update') {
 
   $field = $_POST['campo'] ?? '';
   $valor = isset($_POST['valor']) ? floatval($_POST['valor']) : null;
+  echo "Updating field: $field to value: $valor\n";
   if ($valor === null || $valor < 0) {
     echo json_encode(['error' => 'Cantidad inválida']);
     exit;
