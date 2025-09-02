d3f64ec6a35956bfc645d46b8dd28a8e4599b488
Carlos <carlitosmh1272@gmail.com>
2025-09-02 12:35:12 +0200

    Auto update on save

--- Pages/Componentes/Assets/userAvatar/user.js
+++ Pages/Componentes/Assets/userAvatar/user.js
@@ -1476,7 +1476,6 @@ function toViewMode(rowEl, newValueFormatted, newRaw) {
       <svg class="icon" viewBox="0 0 24 24" fill="none"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z" fill="currentColor"/></svg>
     </button>
   `;
-}
 
 /* Mini-modal de confirmación (promesa). Si ya tienes uno, puedes reutilizarlo */
 function confirmCambios(msg = '¿Estás seguro de aplicar los cambios?') {
