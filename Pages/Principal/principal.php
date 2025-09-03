ad0fa3d9301304264e400011686a813d7a11c9d0
Carlos <carlitosmh1272@gmail.com>
2025-09-03 10:39:10 +0200

    Auto update on save

--- Pages/Principal/principal.php
+++ Pages/Principal/principal.php
@@ -107,6 +107,7 @@ $net = $totalIncome - $totalExpense;
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
 
