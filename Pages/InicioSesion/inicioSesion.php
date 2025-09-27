4629c0685692f4bfef096d95dfc38506d4a3993e
Carlos <carlitosmh1272@gmail.com>
2025-09-27 19:13:06 +0200

    Auto update on save

--- Pages/InicioSesion/inicioSesion.php
+++ Pages/InicioSesion/inicioSesion.php
@@ -1,8 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-session_start();
-
+require_once '../../auth.php'; 
 require_once '../../db.php'; // Incluye la conexión asegurada
 
 // Verificar si la tabla 'usuarios' existe
